0c1cdf2c81bab3f6b7c06bfa46f8361735f0fca8
Giovanalupo <130668804+Giovanalupo@users.noreply.github.com>
2023-09-28 10:08:34 -0300

    Atividade php

---
+++ operadoresdeatribuição.php
@@ -0,0 +1,21 @@
+<?php
+
+//declaração dasa variávreis e atribuições simples
+$SOMAR = 0;
+$numero1 = 2;
+$numero2 = 3;
+$numero3 =4;
+
+//atribuição com adição 
+$SOMAR += $numero1;
+$SOMAR += $numero2;
+
+//ateibuição com multiplicaçaõ 
+$SOMAR *= $numero3;
+
+//atribuição com módulos 
+$SOMAR %= 3;
+
+echo $SOMAR;
+
+?>
